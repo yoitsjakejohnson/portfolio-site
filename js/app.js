ad10e3bafa38190cf96101d6a53d67997c0bf59a
Jake Johnson <itsjakebeats@gmail.com>
2023-11-26 20:47:47 -0600

    Contact form upgrades

--- js/app.js
+++ js/app.js
@@ -1,24 +1,45 @@
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+const navBtn = document.getElementById( "nav-toggle" );
+const links = document.getElementById( "nav-links" );
 
-navBtn.addEventListener("click", () => {
-    navBtn.classList.toggle("is-active");
-    links.classList.toggle("show-links");
-});
+navBtn.addEventListener( "click", () =>
+{
+    navBtn.classList.toggle( "is-active" );
+    links.classList.toggle( "show-links" );
+} );
 
 // set date
-const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
+const date = ( document.getElementById( "date" ).innerHTML = new Date().getFullYear() );
 
 // add box shadow to nav on scroll
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById( "navbar" );
 
-window.onscroll = function () {
-    if (window.scrollY > 22) {
-        navbar.classList.add("shadow-nav");
-    } else {
-        navbar.classList.remove("shadow-nav");
+window.onscroll = function()
+{
+    if ( window.scrollY > 22 )
+    {
+        navbar.classList.add( "shadow-nav" );
+    } else
+    {
+        navbar.classList.remove( "shadow-nav" );
     }
 };
+
+// Get the URL parameters
+const params = new URLSearchParams( window.location.search );
+
+// Check if the status parameter is 'success'
+if ( params.get( 'status' ) === 'success' )
+{
+    // Display a success message using Toastify
+    Toastify( {
+        text: 'Message sent successfully!',
+        duration: 3000,  // Display duration in milliseconds (e.g., 3000ms = 3 seconds)
+        gravity: 'top', // Display position: 'top', 'bottom', 'center'
+        position: 'right', // Display position: 'left', 'right', 'center'
+        backgroundColor: 'green', // Background color of the toast
+        close: true, // Show close button
+    } ).showToast();
+}
 
 // const lightbox = GLightbox({
 //     touchNavigation: true,
